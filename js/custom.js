2be7ab17cc0945e40289cf5b6b96b4c3a7b6878c
Najji09 <n96h09j09@outlook.kr>
2022-04-08 16:45:30 +0900

    class style bind

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,31 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
+    image: './img/animal_camel.png',
+    inStock: false,
+    inventory: 10,
+    details: ['80% cotton', '20% polyester', 'Gender-nuetral'],
+    variants: [
+      {
+        variantID: 2223,
+        variantColor: 'green',
+        variantImage: './img/animal_camel.png',
+      },
+      {
+        variantID: 2224,
+        variantColor: 'blue',
+        variantImage: './img/animal_fox.png',
+      },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+
+    updateProduct: function (variantImage) {
+      this.image = variantImage;
+    },
   },
 });
